3dc23aa94433b58ca9eb698d0d44b5ec7207c14f
alaouiib <alaouiib.fstt@gmail.com>
2021-02-01 21:08:44 +0100

    :first: add 2 string and return their binary sum

---
+++ Add_binary.py
@@ -0,0 +1,10 @@
+
+# Given two binary strings a and b, return their sum as a binary string.
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        return str(bin(int(a, 2) + int(b, 2))[2:])
