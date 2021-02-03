a6b21d10f47240828f6a53479cf1bed9258dd08d
alaouiib <alaouiib.fstt@gmail.com>
2021-02-03 22:20:31 +0100

    find min depth (root to leaf) of a tree

---
+++ min_depth_tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        queue = [(root, 1)]
+
+        while queue:
+            node, depth = queue.pop(0)
+            if not node.left and not node.right:
+                return depth
+            if node.left:
+                queue.append((node.left, depth + 1))
+
+            if node.right:
+                queue.append((node.right, depth + 1))
