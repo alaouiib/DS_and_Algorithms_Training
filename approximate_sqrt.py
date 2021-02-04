dfa63a33c676c4d96f01ee6990f611c06a78bea6
alaouiib <alaouiib.fstt@gmail.com>
2021-02-04 11:17:01 +0100

    :first: approximate sqrt using newton's method

---
+++ approximate_sqrt.py
@@ -0,0 +1,13 @@
+# we will use Newton's method
+# Idea => repeat: compute (Xn+1 = Xn - (f(Xn)/f'(Xn))) until reaching precision (epsilon)
+
+def approximate_sqrt(n: int, epsilon=0.001) -> float:
+    # initialise guess, good value = n/2 (to avoid being on the stationary point or on the other side of the actual sqrt.)
+
+    guess = n/2
+    while (guess**2 - n > epsilon):
+        guess = (guess + n/guess)/2
+    return guess
+
+
+print(approximate_sqrt(10, 0.001))  # => 3.16233, Actual sqrt(10) = 3.1622
