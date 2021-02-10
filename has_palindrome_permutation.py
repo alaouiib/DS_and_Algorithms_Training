7fca70404c6cad458c14752825ae582a33f994f0
alaouiib <alaouiib.fstt@gmail.com>
2021-02-10 22:45:19 +0100

    add problem statement

--- has_palindrome_permutation.py
+++ has_palindrome_permutation.py
@@ -1,3 +1,14 @@
+"""
+Problem: Write an efficient function that checks whether
+ "any" permutation  of an input string is a palindrome. 
+ Examples:
+
+"civic" should return True
+"ivicc" should return True
+"civil" should return False
+"livci" should return False
+"""
+
 
 def has_palindrome_permutation(the_string):
     # Check if any permutation of the input is a palindrome
