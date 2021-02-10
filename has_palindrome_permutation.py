a87fbec5247d97a94e0a1a47689879c667e8b273
alaouiib <alaouiib.fstt@gmail.com>
2021-02-10 22:42:36 +0100

    :first: add has_palindrome_permutation

---
+++ has_palindrome_permutation.py
@@ -0,0 +1,27 @@
+
+def has_palindrome_permutation(the_string):
+    Check if any permutation of the input is a palindrome
+    sol 1 perso
+    l = len(the_string)
+    s = set(the_string)
+    # if the length is odd
+    if l % 2 == 0:
+        if len(s) == l/2:
+            return True
+    # if the length is even
+    elif l % 2 != 0:
+        if len(s) == 1 + (l-1)/2:
+            return True
+    return False
+
+
+def has_palindrome_permutation2(the_string):
+    # sol 2 inspired from sol
+    unique = set()
+    for e in the_string:
+        if e in unique:
+            unique.remove(e)
+        else:
+            unique.add(e)
+
+    return len(unique) <= 1
