4f62386aeedc7639851d5c9b23631e7732be51d1
alaouiib <alaouiib.fstt@gmail.com>
2021-02-20 21:29:06 +0100

    🎉🎉🎉🎉 Add Example of input and output

--- is_first_come_first_served.py
+++ is_first_come_first_served.py
@@ -1,3 +1,14 @@
+"""
+My cake shop is so popular, I'm adding some tables and hiring wait staff
+so folks can have a cute sit-down cake-eating experience.
+
+Example:
+  Take Out Orders: [1, 3, 5]
+  Dine In Orders: [2, 4, 6]
+  Served Orders: [1, 2, 4, 6, 5, 3]
+"""
+
+
 def is_first_come_first_served(take_out_orders, dine_in_orders, served_orders):
 
     # Check if we're serving orders first-come, first-served
