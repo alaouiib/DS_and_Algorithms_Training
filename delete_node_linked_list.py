9fa121de259c8d792eb6f829c407d296cdf24dea
alaouiib <24699882+alaouiib@users.noreply.github.com>
2021-03-14 23:25:13 +0100

    🎉Add comments and exceptions

--- delete_node_linked_list.py
+++ delete_node_linked_list.py
@@ -4,14 +4,15 @@
 
 
 def delete_node(node_to_delete):
+# Get the input node's next node, the one we want to skip to
+next_node = node_to_delete.next
 
-    # Delete the input node from the linked list
-    # all cases : beginning, end, middle and one node in list
-    node_to_delete.value = node_to_delete.next.value
-    node_to_delete.next = node_to_delete.next.next
-    
-    # ## node at the end, only one node.
-    # node_to_delete.value = node_to_delete.next.value
-    # node_to_delete.next = None
-
-
+if next_node:
+    # Replace the input node's value and pointer with the next
+    # node's value and pointer. The previous node now effectively
+    # skips over the input node
+    node_to_delete.value = next_node.value
+    node_to_delete.next  = next_node.next
+else:
+    # Eep, we're trying to delete the last node!
+    raise Exception("Can't delete the last node with this technique!")
