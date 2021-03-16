fd12b4b748fc86133d2c04f925182aeea4d7bd25
alaouiib <24699882+alaouiib@users.noreply.github.com>
2021-03-16 23:20:38 +0100

    Create detect_cycle_linkedlist.py

---
+++ detect_cycle_linkedlist.py
@@ -0,0 +1,4 @@
+## Q: Write a function contains_cycle() that takes the first node in a singly-linked list and returns a boolean indicating whether the list contains a cycle.
+
+def contains_cycle():
+  return false
