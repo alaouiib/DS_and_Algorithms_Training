4145013533a7ab0fb8e449de353df03e8b75f910
alaouiib <24699882+alaouiib@users.noreply.github.com>
2021-03-17 14:53:55 +0100

    🎉add own solution, working for all tests
    test_cycle_loops_to_beginning (__main__.Test) ... ok
    test_cycle_loops_to_middle (__main__.Test) ... ok
    test_empty_list (__main__.Test) ... ok
    test_linked_list_with_no_cycle (__main__.Test) ... ok
    test_one_element_linked_list_cycle (__main__.Test) ... ok
    test_one_element_linked_list_no_cycle (__main__.Test) ... ok
    test_two_node_cycle_at_end (__main__.Test) ... ok

--- detect_cycle_linkedlist.py
+++ detect_cycle_linkedlist.py
@@ -1,4 +1,21 @@
-## Q: Write a function contains_cycle() that takes the first node in a singly-linked list and returns a boolean indicating whether the list contains a cycle.
+## Q: Write a function contains_cycle() that takes the first node in a singly-linked 
+## list and returns a boolean indicating whether the list contains a cycle.
 
-def contains_cycle():
-  return false
+def contains_cycle(first_node):
+
+    # Check if the linked list contains a cycle
+    if first_node:
+        it = first_node.next
+        cache = set()
+        while it:
+            cache.add(it)
+            
+            if it.next == first_node:
+                return True
+            if it.next.next and it.next.next in cache:
+                return True
+            if not it.next.next: return False
+            
+            it = it.next
+        return False
+    else: return False
