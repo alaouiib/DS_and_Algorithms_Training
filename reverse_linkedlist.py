ad4c89bf92af786bf58610f9c09cffe9b9337770
alaouiib <24699882+alaouiib@users.noreply.github.com>
2021-03-17 20:38:25 +0100

    🎉add explanation comments

--- reverse_linkedlist.py
+++ reverse_linkedlist.py
@@ -14,16 +14,21 @@ Q: reverse a linked list
 ## of variables in memory.
 
 def reverse(head_of_list):
-
-    # Reverse the linked list in place
     current_node = head_of_list
     previous_node = None
     next_node = None
+
+    # Until we have 'fallen off' the end of the list
     while current_node:
+        # Copy a pointer to the next element
+        # before we overwrite current_node.next
         next_node = current_node.next
+
+        # Reverse the 'next' pointer
         current_node.next = previous_node
+
+        # Step forward in the list
         previous_node = current_node
         current_node = next_node
-        
 
     return previous_node
