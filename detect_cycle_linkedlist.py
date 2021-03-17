f5e0fb1952d78e13c4a93928de5dca8179314f37
alaouiib <24699882+alaouiib@users.noreply.github.com>
2021-03-17 15:06:23 +0100

    🎉 clean code, remove useless instructions, add complexity.
    ✨Tests passed successfully:
    test_cycle_loops_to_beginning (__main__.Test) ... ok
    test_cycle_loops_to_middle (__main__.Test) ... ok
    test_empty_list (__main__.Test) ... ok
    test_linked_list_with_no_cycle (__main__.Test) ... ok
    test_one_element_linked_list_cycle (__main__.Test) ... ok
    test_one_element_linked_list_no_cycle (__main__.Test) ... ok
    test_two_node_cycle_at_end (__main__.Test) ... ok

--- detect_cycle_linkedlist.py
+++ detect_cycle_linkedlist.py
@@ -1,21 +1,25 @@
 ## Q: Write a function contains_cycle() that takes the first node in a singly-linked 
 ## list and returns a boolean indicating whether the list contains a cycle.
 
+# Complexity: Time O(n), Space O(n):
 def contains_cycle(first_node):
 
     # Check if the linked list contains a cycle
     if first_node:
-        node = first_node.next
+        node = first_node
         cache = set()
         while node:
-            cache.add(node)
+            # If the current node is already in our set, we have a cycle. Return True.
+            if node.next and node.next in cache:
+                return True
+                
+            # If the current node is None we've hit the end of the list. Return False.
+            if not node.next: return False
             
-            if node.next == first_node:
-                return True
-            if node.next.next and node.next.next in cache:
-                return True
-            if not node.next.next: return False
-            # move to the next node
+            # Else throw the current node in our set and keep going.
+            cache.add(node)
+
             node = node.next
         
+    
     return False
