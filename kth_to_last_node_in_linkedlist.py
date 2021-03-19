7edde3ab5e105555ca00b3667905a187976bc64d
alaouiib <24699882+alaouiib@users.noreply.github.com>
2021-03-19 19:05:14 +0100

    🎉add question and class definition

--- kth_to_last_node_in_linkedlist.py
+++ kth_to_last_node_in_linkedlist.py
@@ -1,4 +1,28 @@
 
+# Q: Write a function kth_to_last_node() that takes an integer kk and the head_node of a singly-linked list, and returns the kkth to last node in the list.
 
-def kth_to_last_node_in_linkedlist(head):
+"""
+  class LinkedListNode:
+
+    def __init__(self, value):
+        self.value = value
+        self.next  = None
+
+
+a = LinkedListNode("Angel Food")
+b = LinkedListNode("Bundt")
+c = LinkedListNode("Cheese")
+d = LinkedListNode("Devil's Food")
+e = LinkedListNode("Eccles")
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+# Returns the node with value "Devil's Food" (the 2nd to last node)
+kth_to_last_node(2, a)
+"""
+
+def kth_to_last_node_in_linkedlist(2, head):
   return head
